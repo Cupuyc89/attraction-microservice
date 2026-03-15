f88f170cfb3ca2996de23d69b858536a82aaf811
Aleksandr <aleksandr.karp1989@gmail.com>
2026-03-15 19:27:24 +0400

    feat: create ExcursionControllerTest class and add getById test

--- attraction/src/main/java/ru/pet_projects/attraction/controllers/ExcursionController.java
+++ attraction/src/main/java/ru/pet_projects/attraction/controllers/ExcursionController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 public class ExcursionController {
 
-    private ExcursionRepository excursionRepository;
+    private final ExcursionRepository excursionRepository;
 
     @Autowired
     public ExcursionController(ExcursionRepository excursionRepository) {
