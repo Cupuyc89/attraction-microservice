f0400df7d7a41b831dae5228afe22e0583ee8132
Aleksandr <aleksandr.karp1989@gmail.com>
2026-05-10 21:36:05 +0400

    chore: change Ticket entity

--- ticket-service/src/main/java/ru/pet_projects/ticket_service/entities/Ticket.java
+++ ticket-service/src/main/java/ru/pet_projects/ticket_service/entities/Ticket.java
@@ -2,12 +2,20 @@ package ru.pet_projects.ticket_service.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tickets")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Ticket {
 
     @Id
@@ -31,65 +39,4 @@ public class Ticket {
     @Enumerated(EnumType.STRING)
     private Booking booking;
 
-    public Ticket() {
-    }
-
-    public Ticket(Long id, Long excursionId, Integer price,
-                   LocalDate dateOfStartExcursion,
-                  LocalDate dateOfEndExcursion, Booking booking) {
-        this.id = id;
-        this.excursionId = excursionId;
-        this.price = price;
-        this.dateOfStartExcursion = dateOfStartExcursion;
-        this.dateOfEndExcursion = dateOfEndExcursion;
-        this.booking = booking;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getExcursionId() {
-        return excursionId;
-    }
-
-    public void setExcursionId(Long excursionId) {
-        this.excursionId = excursionId;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public LocalDate getDateOfStartExcursion() {
-        return dateOfStartExcursion;
-    }
-
-    public void setDateOfStartExcursion(LocalDate dateOfStartExcursion) {
-        this.dateOfStartExcursion = dateOfStartExcursion;
-    }
-
-    public LocalDate getDateOfEndExcursion() {
-        return dateOfEndExcursion;
-    }
-
-    public void setDateOfEndExcursion(LocalDate dateOfEndExcursion) {
-        this.dateOfEndExcursion = dateOfEndExcursion;
-    }
-
-    public Booking getBooking() {
-        return booking;
-    }
-
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
 }
